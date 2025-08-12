4fa24465df658ff53c8cd115d1a6f68e4960fad1
durgaprasadchoppa <20072cm008@gmail.com>
2025-08-12 22:42:55 +0530

    Banner

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export const AppContext= createContext();
@@ -6,8 +6,12 @@ export const AppContext= createContext();
 export const AppContextProvider=({children})=>{
     const navigate=useNavigate();
     const [user,setUser] = useState(null)
-    const [isSeller,setisSeller] = useState(false)
-    const value = {navigate,user,setUser,setIsSeller,isSeller}
+    const [isSeller,setIsSeller] = useState(false)
+    const [showUserLogin, setShowUserLogin] = useState(false)
+    const value = {navigate,user,setUser,isSeller,setIsSeller,
+        showUserLogin,setShowUserLogin
+    }
+
     return <AppContext.Provider value={value}>
         {children}
     </AppContext.Provider>
