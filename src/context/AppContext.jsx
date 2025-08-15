ca6c17cd77060dea14c1ac0ec8a83106902a0912
durgaprasadchoppa <20072cm008@gmail.com>
2025-08-15 22:24:28 +0530

    adding pages

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -58,12 +58,34 @@ export const AppContextProvider=({children})=>{
         setCartItems(cartData)
     }
 
+    //get cart count
+    const getCartCount=()=>{
+        let totalCount=0;
+        for(const item in cartItems){
+            totalCount+=cartItems[item];
+        }
+        return totalCount;
+    }
+
+    //get cart total amount
+
+    const getCartAmount=()=>{
+        let totalAmount=0;
+        for(const items in cartItems){
+            let itemInfo = products.find((product)=>product._id === items);
+            if(cartItems[items] > 0){
+                totalAmount += itemInfo.offerPrice * cartItems[items]
+            }
+        }
+        return Math.floor(totalAmount * 100)/100;
+    }
+
     useEffect(()=>{
         fetchPoducts()
     },[])
 
     const value = {navigate,user,setUser,isSeller,setIsSeller,
-        showUserLogin,setShowUserLogin,products,currency,addToCart,updateCartItem,removeFromCart,cartItems,searchQuery,setSearchQuery
+        showUserLogin,setShowUserLogin,products,currency,addToCart,updateCartItem,removeFromCart,cartItems,searchQuery,setSearchQuery,getCartAmount,getCartCount
     }
 
     return <AppContext.Provider value={value}>
